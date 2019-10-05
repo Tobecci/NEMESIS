c4ac76de38dda1ed119f4e3d1bb3e7f7ac7eb451
tobecci <ojinakatochukwu@gmail.com>
2019-10-05 09:47:59 +0100

    changed page title

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Nemesis Twitter Bot Landing Page</title>
+    <title>Nemesis Twitter Bot sign up Page</title>
     <!-- <link rel="stylesheet" href="style.css"> -->
     <link rel="stylesheet" href="register.css">
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
